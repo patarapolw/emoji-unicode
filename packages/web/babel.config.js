73985332168e61a97deb22490f3e7914245d156d
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-10 16:51:29 +0700

    finalize netlify

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    '@babel/preset-typescript',
+    '@babel/preset-env'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-object-rest-spread'
   ]
 }
