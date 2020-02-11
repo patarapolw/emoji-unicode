1c83c8caf71ac8e9538ab57ada0be4b7640d3892
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-02-11 14:18:50 +0700

    add  condition

--- now/api/_get-cond.ts
+++ now/api/_get-cond.ts
@@ -8,65 +8,79 @@ export default function getCond (
     isAny?: boolean
   }>
 ) {
-  return {
-    $and: shlex.split(q).map((el) => {
-      const [op] = /^[-+?]/.exec(el) || [] as string[]
-      if (op) {
-        el = el.substr(1)
-      }
-
-      function addOp (k: string, opK: string, v: any) {
-        if (v && schema) {
-          if (schema[k].type === 'number') {
-            v = parseFloat(v)
-          }
-        }
-
-        if (op === '+') {
-          return { [k]: v }
-        } else if (op === '-') {
-          if (typeof v === 'number' && opK === '>') {
-            v = { [k]: { $lte: v } }
-          } else if (typeof v === 'number' && opK === '<') {
-            v = { [k]: { $gte: v } }
-          } else {
-            v = { $ne: v }
-          }
-
-          return { [k]: v }
-        } else {
-          if (typeof v === 'string') {
-            v = { [k]: { $regex: new RegExp(escapeRegexp(v), 'i') } }
-          } else if (typeof v === 'number' && opK === '>') {
-            v = { [k]: { $gt: v } }
-          } else if (typeof v === 'number' && opK === '<') {
-            v = { [k]: { $lt: v } }
-          } else {
-            v = { [k]: v }
-          }
-
-          if (op === '?') {
-            return { $or: [{}, v] }
-          } else {
-            return v
-          }
-        }
-      }
-
-      const [k, opK, v] = el.split(/([:><])(.+)/)
-
-      if (v) {
-        return addOp(k, opK, v)
-      } else if (schema) {
-        return {
-          $or: Object.keys(schema)
-            .filter((k0) => !schema[k0].type || schema[k0].type === 'string')
-            .filter((k0) => schema[k0].isAny !== false)
-            .map((k0) => addOp(k0, opK, k))
-        }
-      } else {
-        return {}
-      }
-    })
+  const cond = {
+    $or: [] as any[]
   }
+
+  const $and = [] as any[]
+
+  shlex.split(q).map((el) => {
+    const [op] = /^[-+?]/.exec(el) || [] as string[]
+    if (op) {
+      el = el.substr(1)
+    }
+
+    function addOp (k: string, opK: string, v: any) {
+      if (v && schema) {
+        if (schema[k].type === 'number') {
+          v = parseFloat(v)
+        }
+      }
+
+      if (op === '+') {
+        return { [k]: v }
+      } else if (op === '-') {
+        if (typeof v === 'number' && opK === '>') {
+          v = { [k]: { $lte: v } }
+        } else if (typeof v === 'number' && opK === '<') {
+          v = { [k]: { $gte: v } }
+        } else {
+          v = { $ne: v }
+        }
+
+        return { [k]: v }
+      } else {
+        if (typeof v === 'string') {
+          v = { [k]: { $regex: new RegExp(escapeRegexp(v), 'i') } }
+        } else if (typeof v === 'number' && opK === '>') {
+          v = { [k]: { $gt: v } }
+        } else if (typeof v === 'number' && opK === '<') {
+          v = { [k]: { $lt: v } }
+        } else {
+          v = { [k]: v }
+        }
+
+        return v
+      }
+    }
+
+    const [k, opK, v] = el.split(/([:><])(.+)/)
+    let subCond: any = null
+
+    if (v) {
+      subCond = addOp(k, opK, v)
+    } else if (schema) {
+      subCond = {
+        $or: Object.keys(schema)
+          .filter((k0) => !schema[k0].type || schema[k0].type === 'string')
+          .filter((k0) => schema[k0].isAny !== false)
+          .map((k0) => addOp(k0, opK, k))
+          .filter((c) => c)
+      }
+    }
+
+    if (subCond) {
+      if (op === '?') {
+        cond.$or.push(subCond)
+      } else {
+        $and.push(subCond)
+      }
+    }
+  })
+
+  if ($and.length > 0) {
+    cond.$or.push({ $and })
+  }
+
+  return cond
 }
--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,40 +1,43 @@
 <template lang="pug">
-.container(style="margin-top: 1rem;")
+.container(style="padding-top: 1rem; height: 100vh; display: flex; flex-direction: column;")
   b-field(label="Search")
     b-input(v-model="q" placeholder="Please search to view results")
-  b-table(
-    :data="output"
-    paginated
-    :per-page="5"
-    :total = "count"
+  div(style="flex-grow: 1; position: relative;")
+    b-loading(v-if="!output" :is-full-page="false" active)
+    b-table(
+      v-else
+      :data="output"
+      paginated
+      :per-page="5"
+      :total = "count"
 
-    backend-pagination
-    @page-change="page = $event"
-    backend-sorting
-    @sort="onSort"
-    :default-sort="[sort, order]"
-  )
-    template(slot-scope="props")
-      b-table-column(label="Unicode" field="codePoint" sortable width="50")
-        | {{'U+' + props.row.codePoint.toString(16).toLocaleUpperCase()}}
-      b-table-column(label="Symbol" field="symbol" width="100")
-        | {{props.row.symbol}}
-      b-table-column(label="Code" field="code" sortable width="100")
-        code {{props.row.code}}
-      b-table-column(label="Alternatives" field="alt" width="100")
-        div(v-for="a in props.row.alt" :key="a")
-          code {{a}}
-      b-table-column(label="Frequency" field="frequency" sortable width="50")
-        | {{props.row.frequency ? props.row.frequency.toExponential(3) : ''}}
-      b-table-column(label="Description" field="description" sortable) {{props.row.description}}
-      b-table-column(label="Hint" field="hint" style="min-width: 200px;")
-        div(v-for="a, i in props.row.hint" :key="i") {{a}}
-    template(slot="empty")
-      section.section
-        .content.has-text-grey.has-text-centered
-          p
-            b-icon(icon="emoticon-sad" size="is-large")
-          p Nothing here.
+      backend-pagination
+      @page-change="page = $event"
+      backend-sorting
+      @sort="onSort"
+      :default-sort="[sort, order]"
+    )
+      template(slot-scope="props")
+        b-table-column(label="Unicode" field="codePoint" sortable width="50")
+          | {{'U+' + props.row.codePoint.toString(16).toLocaleUpperCase()}}
+        b-table-column(label="Symbol" field="symbol" width="100")
+          | {{props.row.symbol}}
+        b-table-column(label="Code" field="code" sortable width="100")
+          code {{props.row.code}}
+        b-table-column(label="Alternatives" field="alt" width="100")
+          div(v-for="a in props.row.alt" :key="a")
+            code {{a}}
+        b-table-column(label="Frequency" field="frequency" sortable width="50")
+          | {{props.row.frequency ? props.row.frequency.toExponential(3) : ''}}
+        b-table-column(label="Description" field="description" sortable) {{props.row.description}}
+        b-table-column(label="Hint" field="hint" style="min-width: 200px;")
+          div(v-for="a, i in props.row.hint" :key="i") {{a}}
+      template(slot="empty")
+        section.section
+          .content.has-text-grey.has-text-centered
+            p
+              b-icon(icon="emoticon-sad" size="is-large")
+            p Nothing here.
 </template>
 
 <script lang="ts">
@@ -43,9 +46,9 @@ import axios from 'axios'
 
 @Component
 export default class App extends Vue {
-  output: any[] = []
+  output: any[] | null = null
   count = 0
-  q = '+type:emoji'
+  q = ''
 
   get page () {
     const pageString = Array.isArray(this.$route.query.page) ? this.$route.query.page[0] : this.$route.query.page
@@ -93,6 +96,7 @@ export default class App extends Vue {
   }
 
   async created () {
+    this.q = this.$route.query.q as string || '+type:emoji'
     this.load()
   }
 
@@ -107,18 +111,17 @@ export default class App extends Vue {
   @Watch('sort')
   @Watch('order')
   async load () {
-    const q = (Array.isArray(this.q) ? this.q[0] : this.q) || ''
     this.$router.push({
       query: {
         ...this.$route.query,
-        q
+        q: this.q
       }
     })
 
     try {
       const r = await axios.post('/api/search', undefined, {
         params: {
-          q,
+          q: this.q,
           offset: (this.page - 1) * 5,
           sort: this.sort,
           order: this.order
